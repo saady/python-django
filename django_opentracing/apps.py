275a323d6756aad640eeeaa77cde13d7908a4eae
saady <saadyyousef@gmail.com>
2018-01-11 14:51:13 +0000

    Change tracer

--- django_opentracing/apps.py
+++ django_opentracing/apps.py
@@ -1,7 +1,25 @@
 
 from django.apps import AppConfig
+from .db import patch_db
+ 
+def tracer():
+    from jaeger_client import Config
+    config = Config(
+        config={ # usually read from some yaml config
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+            'metrics': True,
+            'enabled': True
+        },
+        service_name='SERVICE_NAME')
+    return config.initialize_tracer()
+
+TRACER = tracer()
 
 class DjangoOpenTracingConfig(AppConfig):
     name = 'django_opentracing'
     def ready(self):
-        pass
+        patch_db(TRACER)
