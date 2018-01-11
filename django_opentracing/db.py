cf0ce53293b506723d65cb2e134d5d67c18c7858
saady <saadyyousef@gmail.com>
2018-01-11 14:04:23 +0000

    complete traced cursor

--- django_opentracing/db.py
+++ django_opentracing/db.py
@@ -1,6 +1,13 @@
+import logging
+
 from django.db import connections
 
 import opentracing
+
+log = logging.getLogger(__name__)
+
+
+CURSOR_ATTR = '_jaeger_cursor'
 
 
 def patch_db(tracer):
@@ -15,8 +22,14 @@ def patch_conn(tracer, conn):
     """
     Patch connection
     """
+    if hasattr(conn, CURSOR_ATTR):
+        log.debug('already patched')
+        return
+
+    setattr(conn, CURSOR_ATTR, conn.cursor)
+
     def cursor():
-        return TracedCursor(tracer, conn)
+        return TracedCursor(tracer, conn, conn._jaeger_cursor())
 
     conn.cursor = cursor
 
@@ -25,9 +38,10 @@ class TracedCursor(object):
     """
     Traced cursor
     """
-    def __init__(self, tracer, conn):
+    def __init__(self, tracer, conn, cursor):
         self.tracer = tracer
         self.conn = conn
+        self.cursor = cursor
 
         self._vendor = getattr(conn, 'vendor', 'db') # mysql
         self._alias = getattr(conn, 'alias', 'default') # default, users
@@ -42,14 +56,34 @@ class TracedCursor(object):
         
     def _trace(self, func, sql, params):
         span = None
-        try:
-            span_ctx = self.tracer.extract(opentracing.Format.HTTP_HEADERS, params)
-            span = self.tracer.start_span(func.__name__, child_of=span_ctx)
+        self.tracer.service_name = self._service
 
-        except (opentracing.InvalidCarrierException, opentracing.SpanContextCorruptedException):
-            span = self.tracer.start_span(func.__name__)
-
+        span = self.tracer.start_span(operation_name=func.__name__)
         span.set_tag(opentracing.ext.tags.DATABASE_STATEMENT, sql)
         span.set_tag(opentracing.ext.tag.DATABASE_TYPE, self._vendor)
         span.set_tag(opentracing.ext.tag.DATABASE_USER, self._alias)
         return span
+
+    def callproc(self, procname, params=None):
+        return self._trace(self.cursor.callproc, procname, params)
+
+    def execute(self, sql, params=None):
+        return self._trace(self.cursor.execute, sql, params)
+
+    def executemany(self, sql, param_list):
+        return self._trace(self.cursor.executemany, sql, param_list)
+
+    def close(self):
+        return self.cursor.close()
+
+    def __getattr__(self, attr):
+        return getattr(self.cursor, attr)
+
+    def __iter__(self):
+        return iter(self.cursor)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
